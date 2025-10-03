644772b882f95fb8d9774f0506da830e07e6e7ba
KietTuan <theonlytruth25012005@gmail.com>
2025-10-03 12:11:48 +0700

    Add backend and frontend initial setup

---
+++ backend/theatermgnt/src/main/java/com/theatermgnt/theatermgnt/TheatermgntApplication.java
@@ -0,0 +1,13 @@
+package com.theatermgnt.theatermgnt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheatermgntApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheatermgntApplication.class, args);
+	}
+
+}
---
+++ backend/theatermgnt/src/test/java/com/theatermgnt/theatermgnt/TheatermgntApplicationTests.java
@@ -0,0 +1,13 @@
+package com.theatermgnt.theatermgnt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheatermgntApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
